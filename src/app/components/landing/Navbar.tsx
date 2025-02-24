67dc51eecd2cd3846c8f5ce1c2cb2d073e2291f9
jittergtz <sandro.gantze@icloud.com>
2025-02-24 16:45:35 +0100

    mobileNav: animation change

--- src/app/components/landing/Navbar.tsx
+++ src/app/components/landing/Navbar.tsx
@@ -134,7 +134,7 @@ const NavigationHome = () => {
           {isMobileMenuOpen && (
             <div className="fixed inset-0 z-50 bg-black bg-opacity-60">
               <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
+                initial={{ opacity: 0, scale: 0.9 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{
                     duration: 0.3,
